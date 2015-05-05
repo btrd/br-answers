a431feaba7627172dc67a5108c10e5ebde8ebe3e
Jean Bertrand <jeanus24@gmail.com>
2015-05-05 15:18:26 +0200

    Delete test

--- app/models/test.rb
+++
@@ -1,8 +0,0 @@
-class User < ActiveRecord::Base
-  has_and_belongs_to_many :sondages
-  has_many :choixes, :through => :reponses
-  before_save :set_mdp
-  def set_mdp
-    self.mdp = SecureRandom.uuid
-  end
-end
